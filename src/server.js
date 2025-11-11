534ea81bc78768941917e210abb0803c43231ee3
lovevarshney2007 <lovevarshney7002@gmail.com>
2025-11-11 16:55:31 +0530

    Set trust proxy option for Express app

--- src/server.js
+++ src/server.js
@@ -21,6 +21,8 @@ connectDB();
 
 const app = express();
 
+app.set("trust proxy", 1);
+
 app.use(express.json({limit : "10kb"}));
 app.use(helmet());
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
